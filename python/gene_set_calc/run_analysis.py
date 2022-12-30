56fa50edc09f92d2c9743b56aa17376a2646b107
Vijay Kumar <vijay.kumar.s@gmail.com>
2022-12-29 21:52:01 -0500

    fix up script

--- python/gene_set_calc/run_analysis.py
+++ python/gene_set_calc/run_analysis.py
@@ -1,21 +1,25 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 import argparse
 import json
 import pickle
 import csv
+import os
 import time
 from collections import defaultdict
 import scipy.sparse as sp_sparse
 import numpy as np
-
+import scanpy
 import gene_set_calc
 
 
 def main(argv):
     parser = argparse.ArgumentParser(description="Process gene sets")
-    parser.add_argument("csr_matrix_pickle", help="CSR count matrix pickle")
+    parser.add_argument(
+        "--csr_matrix_pickle", default=None, help="CSR count matrix pickle"
+    )
+    parser.add_argument("--csr_matrix_h5ad", default=None, help="h5ad matrix")
     parser.add_argument("gene_sets_json", help="gene sets in json")
     parser.add_argument("gene_info_csv", help="gene info csv")
     parser.add_argument(
@@ -29,6 +33,14 @@ def main(argv):
     parser.add_argument("--head", default=0, help="only do top N gene sets")
     args = parser.parse_args(argv[1:])
 
+    if args.csr_matrix_pickle and args.csr_matrix_h5ad:
+        print("Must supply pickle XOR h5ad")
+        sys.exit(1)
+    if not (args.csr_matrix_pickle or args.csr_matrix_h5ad):
+        print("Must supply pickle XOR h5ad")
+        sys.exit(1)
+
+    start_load = time.time()
     with open(args.gene_sets_json, "r") as fin:
         gene_sets = json.load(fin)
 
@@ -39,17 +51,36 @@ def main(argv):
         for i, row in enumerate(reader):
             name = row["gene_symbol"]
             gene_symbols[name].append(i)
+            num_rows += 1
 
-    with open(args.csr_matrix_pickle, "rb") as fin:
-        csr_mat = pickle.load(fin)
+    csr_mat = None
 
+    if args.csr_matrix_pickle:
+        print("Reading csr matrix from pickle")
+        with open(args.csr_matrix_pickle, "rb") as fin:
+            csr_mat = pickle.load(fin)
+    else:
+        ts_h5ad = scanpy.read_h5ad(args.csr_matrix_h5ad)
+        csr_mat = ts_h5ad.X.transpose().tocsr()
+
+        # pickle_path = f"{args.csr_matrix_h5ad}.csr_matrix.pickle"
+        # if not os.path.exists(pickle_path):
+        #     with open(pickle_path, "wb") as out:
+        #         pickle.dump(csr_mat, out)
+        #         print(f"Wrote CSR matrix pickle to {pickle_path}")
+        #         sys.exit(0)
+        # else:
+        #     print(f"pickle path already exists: {pickle_path}")
+        #     sys.exit(1)
+
+    assert csr_mat is not None
     num_genes, num_cells = csr_mat.shape
     assert num_rows == num_genes
     rust_data = csr_mat.data.astype(np.float32)
     rust_indices = csr_mat.indices.astype(np.uint64)
     rust_indptr = csr_mat.indptr.astype(np.uint64)
 
-    print("Done loading")
+    print(f"Done loading {time.time()-start_load:.1f}s")
 
     for key, val in gene_sets.items():
         gene_names = val["geneSymbols"]
